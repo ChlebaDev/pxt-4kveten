24c74ab2b24738cd777fb0f4945d4594c6c956ff
ChlebaDev <115696170+ChlebaDev@users.noreply.github.com>
2023-05-09 23:23:39 +0200

    nevimcodelam

--- main.ts
+++ main.ts
@@ -1,42 +1,39 @@
 // radio.setGroup(10)
 
 
+radio.onReceivedString(function (str: string) {
 
-const rychlost = 238
+const jakMocJsemOtocilTroubu = ("str".charCodeAt(0))
 
 function autickoJede(ul: number = 0, ur: number = 0, ll: number = 0, lr: number = 0) {
     ul = Math.map(ll, -100, 100, -255, 255)
     ur = Math.map(lr, -100, 100, -215, 215)
     PCAmotor.MotorRun(PCAmotor.Motors.M1, -ul)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -ur)
-}
+    }
 
 autickoJede(-100, -100, 0, 0)
 autickoJede(-100, -100)
 
 function autickoDozadu() {
-    autickoJede(-100 * rychlost, -100 * rychlost,
-                -100 * rychlost, -100 * rychlost)
+    autickoJede(-100 * jakMocJsemOtocilTroubu, -100 * jakMocJsemOtocilTroubu,
+                -100 * jakMocJsemOtocilTroubu, -100 * jakMocJsemOtocilTroubu)
 
-}
+    }
 function autickoDopredu() {
-    autickoJede(100 * rychlost, 100 * rychlost,
-                100 * rychlost, 100 * rychlost)
+    autickoJede(100 * jakMocJsemOtocilTroubu, 100 * jakMocJsemOtocilTroubu,
+                100 * jakMocJsemOtocilTroubu, 100 * jakMocJsemOtocilTroubu)
 
-}
+    }
 
-
-/*
-radio.onReceivedNumber(function(receivedNumber: number) {
+    if (jakMocJsemOtocilTroubu > 0) {   
+        return autickoDopredu
+    } else {
+        return autickoDozadu
+    }
 
 })
 
-radio.onReceivedValue(function(name: string, value: number) {
+  
     
-})
-*/
-
-radio.onReceivedString(function(str: string) {
-    console.log(str)
-    
-})
+// ("str".charCodeAt(0))
