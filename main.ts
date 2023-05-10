76c7bc91b24a2f3489628c694043e2c3b56b8f77
ChlebaDev <115696170+ChlebaDev@users.noreply.github.com>
2023-05-10 13:30:57 +0200

    upravana2kola

--- main.ts
+++ main.ts
@@ -1,32 +1,40 @@
 // radio.setGroup(10)
 
 
+
+
+
+
+
+
 radio.onReceivedString(function (str: string) {
 
 const jakMocJsemOtocilTroubu = ("str".charCodeAt(0))
 
-function autickoJede(ul: number = 0, ur: number = 0, ll: number = 0, lr: number = 0) {
-    ul = Math.map(ll, -100, 100, -255, 255)
-    ur = Math.map(lr, -100, 100, -215, 215)
+function autickoJede(lw: number = 0, rw: number = 0) {
+    const ul = Math.map(lw, -100, 100, -255, 255) // přizpůsobit našem kolům
+    const ur = Math.map(rw, -100, 100, -215, 215)
     PCAmotor.MotorRun(PCAmotor.Motors.M1, -ul)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -ur)
     }
 
-autickoJede(-100, -100, 0, 0)
+
+/*
 autickoJede(-100, -100)
+autickoJede(0, 0)
+*/
+
 
 function autickoDozadu() {
-    autickoJede(-100 * jakMocJsemOtocilTroubu, -100 * jakMocJsemOtocilTroubu,
-                -100 * jakMocJsemOtocilTroubu, -100 * jakMocJsemOtocilTroubu)
+    autickoJede(-1 * jakMocJsemOtocilTroubu, -1 * jakMocJsemOtocilTroubu)
 
     }
 function autickoDopredu() {
-    autickoJede(100 * jakMocJsemOtocilTroubu, 100 * jakMocJsemOtocilTroubu,
-                100 * jakMocJsemOtocilTroubu, 100 * jakMocJsemOtocilTroubu)
+    autickoJede(1 * jakMocJsemOtocilTroubu, 1 * jakMocJsemOtocilTroubu)
 
     }
 
-    if (jakMocJsemOtocilTroubu > 0) {   
+    if (jakMocJsemOtocilTroubu > 127) {
         return autickoDopredu
     } else {
         return autickoDozadu
